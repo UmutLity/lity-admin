d36689ea9fdda769d2b8c28581b3a6f895ebe569
UmutLity <umut1015lity@gmail.com>
2026-04-16 00:56:47 +0300

    fix: allow public api cors for both litysoftware domains

--- next.config.js
+++ next.config.js
@@ -62,7 +62,7 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.litysoftware.com",
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
